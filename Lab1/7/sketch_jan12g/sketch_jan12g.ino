6641e33386bbea9477f7a5cb72753349e65738e3
h3xadecima1 <hexadecimal512@gmail.com>
2025-01-13 13:14:46 -0700

    update

--- Lab1/7/sketch_jan12g/sketch_jan12g.ino
+++ Lab1/7/sketch_jan12g/sketch_jan12g.ino
@@ -1,35 +1,33 @@
 // this program is a simple Arduino Calculator calculator
-#define btnPin 2
-#define btnPin2 3
+#define buttonPin 2
 #define RED_LED 6
 #define GREEN_LED 7
 #define BLUE_LED 8
 #define ARRAY_SIZE 10
-uint8_t btn_prev;
-uint8_t btn2_prev;
-int counter = 0;
+
+int counter = 0;  // counter for the number of button presses
+int buttonState = 0;        // current state of the button
+int lastButtonState = 0;
+
 int operation = 0;
 int resultArray[ARRAY_SIZE];
 int array1[ARRAY_SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 int array2[ARRAY_SIZE] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
 void setup () {
-  pinMode(btnPin, INPUT_PULLUP);
-  pinMode(btnPin2, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT);
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
   btn_prev = digitalRead(btnPin);
-  btn2_prev= digitalRead(btnPin2); 
 }
 
 void loop() 
 {
-  uint8_t btn = digitalRead(btnPin);
-  uint8_t btn2 = digitalRead(btnPin2);
-
-  if (btn == LOW && btn_prev == HIGH) 
+  buttonState = digitalRead(buttonPin)
+  if (buttonState != lastButtonState) {
   {
+    if (buttonState == HIGH) {
       counter++;
       if (counter == 1) 
       {
@@ -47,9 +45,10 @@ void loop()
           operation = 3; // Multiplication
       }
       counter %= 3; // Reset counter after 3 presses
+    }
   }
 
-  if (btn2 == LOW && btn2_prev == HIGH && operation != 0) 
+  if (btn == LOW && btn_prev == HIGH && operation != 0) 
   {
     for (int i = 0; i < ARRAY_SIZE; i++) 
     {
