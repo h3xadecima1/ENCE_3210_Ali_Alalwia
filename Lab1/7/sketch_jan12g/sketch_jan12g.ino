2eab81a04ea10ac94b009d4476aedd938e426a97
h3xadecima1 <hexadecimal512@gmail.com>
2025-01-16 19:53:21 -0700

    update

--- Lab1/7/sketch_jan12g/sketch_jan12g.ino
+++ Lab1/7/sketch_jan12g/sketch_jan12g.ino
@@ -1,17 +1,17 @@
-#define selectButton 9
-#define runButton 10
-#define redLed 11
-#define greenLed 12
-#define blueLed 13
+#define selectButton 9   // Button for selecting the operation
+#define runButton 10     // Button for triggering the operation
+#define redLed 11        // Red LED to indicate operation
+#define greenLed 12      // Green LED to indicate operation
+#define blueLed 13       // Blue LED to indicate operation
 
-int buttonState = 0;        // current state of the button
-int lastButtonState = 0;
-int arr1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-int arr2[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-int arr3[10] = {};
-int counter = 0;
-void setup() 
-{
+int arr1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};  // First array
+int arr2[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};  // Second array
+int result[10] = {};  // Array to store results of operations
+int counter = 0;       // Track number of button presses
+int buttonState = 0;   // Current state of the select button
+int lastButtonState = 0;  // Previous state of the select button
+
+void setup() {
   Serial.begin(9600);
   pinMode(selectButton, INPUT);
   pinMode(runButton, INPUT);
@@ -21,51 +21,63 @@ void setup()
 }
 
 void loop() {
-  if (digitalRead(selectButton) == HIGH);
-  {
-    counter +=1;
-    Serial.println(counter);
-    if (counter == 1)
-    {
-      digitalWrite(redLed, HIGH);
+  // Check if the select button is pressed to change the operation
+  buttonState = digitalRead(selectButton);
+  
+  // Detect button press (LOW to HIGH transition for selectButton)
+  if (buttonState == HIGH && lastButtonState == LOW) {
+    delay(50);  // Debounce delay
+    counter++;
+    if (counter > 3) {
+      counter = 1;  // Reset counter after 3 clicks
     }
-    else if (counter == 2)
-    {
-      digitalWrite(greenLed, HIGH);
-    }
-    else if(counter == 3)
-    {
-      digitalWrite(blueLed , HIGH);
+
+    // Update the LEDs based on the counter (operation type)
+    digitalWrite(redLed, (counter == 1) ? HIGH : LOW);
+    digitalWrite(greenLed, (counter == 2) ? HIGH : LOW);
+    digitalWrite(blueLed, (counter == 3) ? HIGH : LOW);
+    
+    // Print the current operation type for debugging
+    Serial.print("Operation mode: ");
+    if (counter == 1) {
+      Serial.println("Add");
+    } else if (counter == 2) {
+      Serial.println("Subtract");
+    } else if (counter == 3) {
+      Serial.println("Multiply");
     }
   }
 
-  if(digitalRead(runButton) == HIGH)
-  {
-      digitalWrite(redLed, LOW);
-      digitalWrite(greenLed, LOW);
-      digitalWrite(blueLed, LOW);
-      Serial.print(operationMatrix(arr1[10],  arr2[10],  arr3[10], counter));
-      delay(3000);
-      digitalWrite(redLed, HIGH);
-      digitalWrite(greenLed, HIGH);
-      digitalWrite(blueLed, HIGH);
+  // If the run button is pressed, perform the operation
+  if (digitalRead(runButton) == HIGH) {
+    // Perform the operation based on the current counter value
+    operationMatrix(arr1, arr2, result, counter);
+    
+    // Print the results for debugging
+    Serial.println("Result:");
+    for (int i = 0; i < 10; i++) {
+      Serial.print(result[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
+    
+    // Delay for a while to prevent rapid retriggering of the run button
+    delay(3000);
   }
+
+  // Update the last button state
+  lastButtonState = buttonState;
 }
 
-int operationMatrix(int arrA[10], int arrB[10], int result[10], int counter){
-  for (int i = 0 ; i<10 ; i++)
-  {
-    if (counter == 1)
-    {
-      result[i] = arrA[i] + arrB[i];
-    }
-    else if (counter == 2)
-    {
-      result[i] = arrA[i] - arrB[i];
-    }
-    else if (counter == 3)
-    {
-      result[i] = arrA[i] + arrB[i];
+// Function to perform the operation based on the counter value
+void operationMatrix(int arrA[], int arrB[], int result[], int counter) {
+  for (int i = 0; i < 10; i++) {
+    if (counter == 1) {
+      result[i] = arrA[i] + arrB[i];  // Add
+    } else if (counter == 2) {
+      result[i] = arrA[i] - arrB[i];  // Subtract
+    } else if (counter == 3) {
+      result[i] = arrA[i] * arrB[i];  // Multiply
     }
   }
 }
