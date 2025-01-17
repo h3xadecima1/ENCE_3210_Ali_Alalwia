badfb0ac933002f6e66bacdcb3b7e7204977a40f
h3xadecima1 <hexadecimal512@gmail.com>
2025-01-17 10:21:36 -0700

    update

--- Lab2/1/sketch_jan17a/sketch_jan17a.ino
+++ Lab2/1/sketch_jan17a/sketch_jan17a.ino
@@ -1,9 +1,48 @@
-void setup() {
-  // put your setup code here, to run once:
+#define GREEN_LED 4
+#define RED_LED 5
+#define BUTTON_1 3
+#define BUTTON_2 2
+static volatile int counter_red = 0;
+static volatile int counter_green = 0;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_1), isr_button1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_2), isr_button2, CHANGE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(counter_red > counter_green)
+  {
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(GREEN_LED, LOW);
+    delay(500);
 
+  }
+  else if(counter_green > counter_red)
+  {
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(RED_LED, LOW);
+    delay(500);
+  }
+  else if (counter_red == counter_green){
+    Serial.println("I'm Blue");
+  }
+}
+
+void isr_button1()
+{
+  if(digitalRead(BUTTON_1))
+  {
+    counter_red++;
+  }
+}
+void isr_button2()
+{
+  if (digitalRead(BUTTON_2))
+  {
+    counter_green++;
+  }
 }
--- Lab2/2/sketch_jan17b/sketch_jan17b.ino
+++ Lab2/2/sketch_jan17b/sketch_jan17b.ino
@@ -1,9 +1,48 @@
-void setup() {
-  // put your setup code here, to run once:
+// LED Sequence Program
+// Mask for PORTB to turn the first 3 bits on 00000111
+#define PORTB_MASK 0x07
+#define BUTTON_1
+#define BUTTON_2
+// Data Direction Register for port B
+#define myDDRB (*(volatile uint8_t *) 0x24)   
+// Data Register for port B
+#define myPORTB (*(volatile uint8_t *) 0x25)
+// Input Register for port B
+#define myPINB (*(volatile uint8_t *) 0x23)
 
+// Array to control the LED Sequence
+int arr[6] = {0x04, 0x06, 0x07, 0x03, 0x01, 0x05};
+
+void setup() {
+  // Set the first 3 pins of port B as output (00000111)
+  myDDRB |= PORTB_MASK;
+  
+  // Initialize PORTB to 0 (turn off all LEDs initially)
+  myPORTB &= ~PORTB_MASK;
+  attachInterrupt(digitalPinToInterrupt(BUTTON_1), isr_button1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_2), isr_button2, CHANGE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Check if the button on PINB3 is pressed
+  if (myPINB & (1 << PINB3)) {
+    int i = 0;
+    
+    while (i < 6) {
+      if (myPORTB == 0x00) {
+        // Turn on the LED pattern
+        myPORTB ^= arr[i];
+        delay(1000);  // Wait for 1 second
+        // Turn off the LED pattern
+        myPORTB ^= arr[i];
+        delay(1000);  // Wait for 1 second
+        
+        // Move to the next pattern in the array
+        i++;
+      }
+      
+      // Ensure i stays within the bounds of the array
+      i = i % 6;
+    }
+  }
 }
