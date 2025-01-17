06c2bb3e52b9d28b64ded37c83f772206d42f1d5
h3xadecima1 <hexadecimal512@gmail.com>
2025-01-16 19:31:11 -0700

    update

--- Lab1/6/sketch_jan12f/sketch_jan12f.ino
+++ Lab1/6/sketch_jan12f/sketch_jan12f.ino
@@ -1,30 +1,45 @@
-//LED Sequence Program
-//Mask for PORTB to turn the first 3 bits on 00000111
-#define PORTB5 0x07
+// LED Sequence Program
+// Mask for PORTB to turn the first 3 bits on 00000111
+#define PORTB_MASK 0x07
+
 // Data Direction Register for port B
 #define myDDRB (*(volatile uint8_t *) 0x24)   
-// Data register for port B
+// Data Register for port B
 #define myPORTB (*(volatile uint8_t *) 0x25)
-//Array to control the LED Sequence
+// Input Register for port B
+#define myPINB (*(volatile uint8_t *) 0x23)
+
+// Array to control the LED Sequence
 int arr[6] = {0x04, 0x06, 0x07, 0x03, 0x01, 0x05};
 
 void setup() {
-  myDDRB &= PORTB5;
-  myPORTB &= 0x00;
+  // Set the first 3 pins of port B as output (00000111)
+  myDDRB |= PORTB_MASK;
+  
+  // Initialize PORTB to 0 (turn off all LEDs initially)
+  myPORTB &= ~PORTB_MASK;
 }
+
 void loop() {
-  if (PINB ^(0<<PINB3)){
+  // Check if the button on PINB3 is pressed
+  if (myPINB & (1 << PINB3)) {
     int i = 0;
-    while (i < 6)
-    {
-      if(myPORTB == 0x00){
-      myPORTB ^= arr[i];
-      delay(1000);
-      myPORTB ^= arr[i];
-      delay(1000);
-      i++;
-      i = i % 6;
+    
+    while (i < 6) {
+      if (myPORTB == 0x00) {
+        // Turn on the LED pattern
+        myPORTB ^= arr[i];
+        delay(1000);  // Wait for 1 second
+        // Turn off the LED pattern
+        myPORTB ^= arr[i];
+        delay(1000);  // Wait for 1 second
+        
+        // Move to the next pattern in the array
+        i++;
       }
+      
+      // Ensure i stays within the bounds of the array
+      i = i % 6;
     }
   }
 }
--- Lab1/7/sketch_jan12g/sketch_jan12g.ino
+++ Lab1/7/sketch_jan12g/sketch_jan12g.ino
@@ -1,77 +1,71 @@
-// this program is a simple Arduino Calculator calculator
-#define buttonPin 2
-#define RED_LED 6
-#define GREEN_LED 7
-#define BLUE_LED 8
-#define ARRAY_SIZE 10
+#define selectButton 9
+#define runButton 10
+#define redLed 11
+#define greenLed 12
+#define blueLed 13
 
-int counter = 0;  // counter for the number of button presses
 int buttonState = 0;        // current state of the button
 int lastButtonState = 0;
-
-int operation = 0;
-int resultArray[ARRAY_SIZE];
-int array1[ARRAY_SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-int array2[ARRAY_SIZE] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-
-void setup () {
-  pinMode(buttonPin, INPUT);
-  pinMode(RED_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(BLUE_LED, OUTPUT);
-  btn_prev = digitalRead(btnPin);
+int arr1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int arr2[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int arr3[10] = {};
+int counter = 0;
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(selectButton, INPUT);
+  pinMode(runButton, INPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(blueLed, OUTPUT);
 }
 
-void loop() 
-{
-  buttonState = digitalRead(buttonPin)
-  if (buttonState != lastButtonState) {
+void loop() {
+  if (digitalRead(selectButton) == HIGH);
   {
-    if (buttonState == HIGH) {
-      counter++;
-      if (counter == 1) 
-      {
-          digitalWrite(RED_LED, HIGH);
-          operation = 1; // Addition
-      } 
-      else if (counter == 2) 
-      {
-          digitalWrite(GREEN_LED, HIGH);
-          operation = 2; // Subtraction
-      } 
-      else if (counter == 3) 
-      {
-          digitalWrite(BLUE_LED, HIGH);
-          operation = 3; // Multiplication
-      }
-      counter %= 3; // Reset counter after 3 presses
+    counter +=1;
+    Serial.println(counter);
+    if (counter == 1)
+    {
+      digitalWrite(redLed, HIGH);
+    }
+    else if (counter == 2)
+    {
+      digitalWrite(greenLed, HIGH);
+    }
+    else if(counter == 3)
+    {
+      digitalWrite(blueLed , HIGH);
     }
   }
 
-  if (btn == LOW && btn_prev == HIGH && operation != 0) 
+  if(digitalRead(runButton) == HIGH)
   {
-    for (int i = 0; i < ARRAY_SIZE; i++) 
+      digitalWrite(redLed, LOW);
+      digitalWrite(greenLed, LOW);
+      digitalWrite(blueLed, LOW);
+      Serial.print(operationMatrix(arr1[10],  arr2[10],  arr3[10], counter));
+      delay(3000);
+      digitalWrite(redLed, HIGH);
+      digitalWrite(greenLed, HIGH);
+      digitalWrite(blueLed, HIGH);
+  }
+}
+
+int operationMatrix(int arrA[10], int arrB[10], int result[10], int counter){
+  for (int i = 0 ; i<10 ; i++)
+  {
+    if (counter == 1)
     {
-      switch (operation) 
-      {
-        case 1:
-            resultArray[i] = array1[i] + array2[i];
-            break;
-        case 2:
-            resultArray[i] = array1[i] - array2[i];
-            break;
-        case 3:
-            resultArray[i] = array1[i] * array2[i];
-            break;
-      }
+      result[i] = arrA[i] + arrB[i];
     }
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(GREEN_LED, LOW);
-    digitalWrite(RED_LED, LOW);
-    delay(1000);
-    operation = 0;
-    digitalWrite(RED_LED, HIGH);
-    digitalWrite(GREEN_LED, HIGH);
-    digitalWrite(RED_LED, HIGH);
+    else if (counter == 2)
+    {
+      result[i] = arrA[i] - arrB[i];
+    }
+    else if (counter == 3)
+    {
+      result[i] = arrA[i] + arrB[i];
+    }
   }
 }
