4cb15025b3c9b6adff5ef9cb100899fb7e97b02e
h3xadecima1 <hexadecimal512@gmail.com>
2025-01-23 20:00:00 -0700

    update

--- Lab2/1/sketch_jan17a/sketch_jan17a.ino
+++ Lab2/1/sketch_jan17a/sketch_jan17a.ino
@@ -2,6 +2,8 @@
 #define GREEN_LED 4
 // green led pin definition
 #define RED_LED 5
+// Blue led pin definition
+#define BLUE_LED 6
 // button 1 pin definition
 #define BUTTON_1 3
 // button 2 pin definition
@@ -14,9 +16,11 @@ static volatile int gCounter_green = 0;
 void setup() {
   // enable serial protocol at 9600 baud rate
   Serial.begin(9600);
-  // set pin mode for the red led to output
+  // set pin mode for the Blue led to output
+  pinMode(BLUE_LED, OUTPUT);
+  // set pin mode for the Green led to output
   pinMode(GREEN_LED, OUTPUT);
-  // set pin mode for the green led to output
+  // set pin mode for the red led to output
   pinMode(RED_LED, OUTPUT);
   // attach an interrupt to the Button 1 that will be called on button press
   attachInterrupt(digitalPinToInterrupt(BUTTON_1), isr_button1, CHANGE);
@@ -28,6 +32,8 @@ void loop() {
   // condition compares if button 1 is pressed more than button 2
   if(gCounter_red > gCounter_green)
   {
+    //Turns off the blue led in case it was on due to a previous state
+    digitalWrite(BLUE_LED, LOW);
     // turn on the red led if the times that button 1 is pressed more than the times button 2 is pressed
     digitalWrite(RED_LED, HIGH);
     // turn off the green led if the times that button 1 is pressed more than the times button 2 is pressed
@@ -39,7 +45,9 @@ void loop() {
   // condition compares if button 2 is pressed more than button 1
   else if(gCounter_green > gCounter_red)
   {
-    // turn on the red led if the times that button 2 is pressed more than the times button 1 is pressed
+    //Turns off the blue led in case it was on due to a previous state
+    digitalWrite(BLUE_LED, LOW);
+    // turn on the Green led if the times that button 2 is pressed more than the times button 1 is pressed
     digitalWrite(GREEN_LED, HIGH);
     // turn off the red led if the times that button 2 is pressed more than the times button 1 is pressed
     digitalWrite(RED_LED, LOW);
@@ -48,9 +56,14 @@ void loop() {
   }
   // checks if both buttons 1 and 2 eually pressed 
   else if (gCounter_red == gCounter_green){
-    /* if the both buttons were pressed same numbers then blue led should turn on
-    since there is no blue led in the board a serial print value "I'm Blue" was implemented instad */
-    Serial.println("I'm Blue");
+    // turn off the green if the times that button 2 is pressed more than the times button 1 is pressed
+    digitalWrite(GREEN_LED, HIGH);
+    // turn off the red led if the times that button 2 is pressed more than the times button 1 is pressed
+    digitalWrite(RED_LED, LOW);
+    // delay of half a second 
+    // if the both buttons were pressed same numbers then blue led turns on
+    digitalWrite(BLUE_LED, HIGH);
+    delay(500)
   }
 }
 // Button 1 interrupt will only run if button 1 is pressed
